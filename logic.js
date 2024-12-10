3c624e73504614c3fbe81b19a8d83b10e2110c6f
Tormihunt <tormihunt@gmail.com>
2024-12-10 16:58:21 +0200

    Add display functionality
    Clicking a number will display it

--- logic.js
+++ logic.js
@@ -1,9 +1,15 @@
 //all querySelectors
 const numberContainer = document.querySelector("#numberContainer")
+const display = document.querySelector("#display")
 
 for (let i = 9; i > 0; i--) {
-    let div = document.createElement("div")
-    div.setAttribute("id", `div${i}`)
-    div.textContent = i
-    numberContainer.appendChild(div)
+    let btn = document.createElement("btn")
+    btn.setAttribute("id", `div${i}`)
+    btn.textContent = i
+    numberContainer.appendChild(btn)
 }
+
+numberContainer.addEventListener("click", (event) => {
+    source = event.target
+    display.textContent = source.textContent
+})
